9fb272afaf90c5817d342f140aabe4c456cbd977
narsimaxwell <narsijangid01@gmail.com>
2024-03-13 20:12:21 +0530

    changes

---
+++ hooks/useSetFormErrors.ts
@@ -0,0 +1,89 @@
+// All comments are related for strong password
+
+
+import React from 'react';
+import {
+  emailValidate,
+  usernameValidate,
+} from '../util/validate';
+
+interface Props {
+  email: string;
+  password: string;
+  username?: string;
+  setEmailFormErrors: React.Dispatch<React.SetStateAction<string>>;
+  // setPasswordFormErrors: React.Dispatch<React.SetStateAction<string>>;
+  setUsernameFormErrors: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function useSetFormErrors({
+  email,
+  password,
+  username,
+  setEmailFormErrors,
+  // setPasswordFormErrors,
+  setUsernameFormErrors,
+}: Props) {
+  React.useEffect(() => {
+    setEmailFormErrors(emailValidate(email));
+    // setPasswordFormErrors(passwordValidate(password));
+    if (username) {
+      setUsernameFormErrors(usernameValidate(username));
+    }
+  }, [
+    email,
+    password,
+    username,
+    setEmailFormErrors,
+    // setPasswordFormErrors,
+    setUsernameFormErrors,
+  ]);
+}
+
+export default useSetFormErrors;
+
+
+
+
+
+// import React from 'react';
+// import {
+//   emailValidate,
+//   passwordValidate,
+//   usernameValidate,
+// } from '../util/validate';
+
+// interface Props {
+//   email: string;
+//   password: string;
+//   username?: string;
+//   setEmailFormErrors: React.Dispatch<React.SetStateAction<string>>;
+//   setPasswordFormErrors: React.Dispatch<React.SetStateAction<string>>;
+//   setUsernameFormErrors: React.Dispatch<React.SetStateAction<string>>;
+// }
+
+// function useSetFormErrors({
+//   email,
+//   password,
+//   username,
+//   setEmailFormErrors,
+//   setPasswordFormErrors,
+//   setUsernameFormErrors,
+// }: Props) {
+//   React.useEffect(() => {
+//     setEmailFormErrors(emailValidate(email));
+//     setPasswordFormErrors(passwordValidate(password));
+//     if (username) {
+//       setUsernameFormErrors(usernameValidate(username));
+//     }
+//   }, [
+//     email,
+//     password,
+//     username,
+//     setEmailFormErrors,
+//     setPasswordFormErrors,
+//     setUsernameFormErrors,
+//   ]);
+// }
+
+// export default useSetFormErrors;
---
+++ util/guestAccess.ts
@@ -0,0 +1,8 @@
+const guestAccess = () => {
+  const email = 'guest@gmail.com';
+  const password = '12345678';
+
+  return { email, password };
+};
+
+export default guestAccess;
